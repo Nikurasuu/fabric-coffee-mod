a7e062b154fc6f12405605332617d2f8126326d0
Niklas Malkusch <niklasmalkusch@googlemail.com>
2023-11-11 22:10:19 +0100

    Remove maxCount limit for Bean item in constructor

--- src/main/java/net/vielleichtNiklas/Bean.java
+++ src/main/java/net/vielleichtNiklas/Bean.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Item;
 
 public class Bean extends Item {
     public Bean(Settings settings) {
-        super(settings.food(Bean.FOOD_COMPONENT).maxCount(128));
+        super(settings.food(Bean.FOOD_COMPONENT));
     }
 
     public static final FoodComponent FOOD_COMPONENT = (new FoodComponent.Builder()).hunger(2).saturationModifier(0.1F).build();
